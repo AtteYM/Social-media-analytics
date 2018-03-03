37146a1d521f0ea0fdcea8a959b6761a41c7d06e
Atte Ylä-Mattila <atte.ym@gmail.com>
2018-03-03 19:58:55 +0200

    Main-function and running script
    Added main function to hashtags.py and ran it with Tampere3_Luuppi.xlsx data generating new file "data2.xlsx"

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Hashtags.py is for going through Twitter data stored in xlsx-file.
 Purpose is to extract different tags used in tweets and count frequence for
 them. After this data is stored in new xlsx-file where it is easy to turn in to
 table and sort descending.
+
+Tampere3_Luuppi.xlsx contains data for hashtags.py and data2.xlsx contains tags
+from Tampere3_Luuppi.xlsx-file.
--- hashtags.py
+++ hashtags.py
@@ -1,4 +1,6 @@
-
+from openpyxl import load_workbook
+import collections
+import xlsxwriter
 
 #Goes through string and makes a list containing all tags which start with '#'
 def get_hashtagslist(string):
@@ -11,8 +13,8 @@ def get_hashtagslist(string):
             if s:
                 ret.append(s)
                 s=''
-#            continue
-#remove previous commentmark if you want to exclude # from words
+ #           continue
+ #remove previous commentmark if you want to exclude # from words
 
         #define characters that aren´t included in tags
         #in other words which characters define end of tag
@@ -30,3 +32,41 @@ def get_hashtagslist(string):
 
     #return list of tags used in tweet
     return list(set([word for word in ret if len(ret)>1 and len(ret)<20]))
+
+def main():
+
+    #open specific workbook and sheet
+    wb = load_workbook('Tampere3_Luuppi.xlsx')
+    ws = wb['DataTaulu']
+
+    htags = []
+
+    #save column and go through specific rows containing tweets
+    colC = ws['C']
+    for i in range(1,1728):
+        #for cleaning the data make words lowercase
+        tweet = str(colC[i].value).lower()
+        htags = htags + get_hashtagslist(tweet)
+
+    #turn list in to counter to group tags and calculate frequency
+    counter = collections.Counter(htags)
+
+    #print most common tags and amount of unique tags
+    #print(counter.most_common(10))
+    #print(len(counter))
+
+    #write tags and frequencies in to first two colums of a new file
+    workbook = xlsxwriter.Workbook('data2.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    row = 0
+    col = 0
+
+    for key in counter.keys():
+        row += 1
+        worksheet.write(row, col, key)
+        worksheet.write(row, col + 1, counter[key])
+
+    workbook.close()
+
+main()
